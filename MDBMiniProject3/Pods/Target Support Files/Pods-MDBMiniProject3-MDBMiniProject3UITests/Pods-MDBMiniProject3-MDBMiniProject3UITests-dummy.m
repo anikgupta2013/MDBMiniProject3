27a71dd54a8e89cdc7938e79b3f594e8c6ba5a96
Anik Gupta <anik.gupta@gmail.com>
2019-09-25 18:52:02 -0700

    Added Firebase and Cocoapods

---
+++ MDBMiniProject3/Pods/Target Support Files/Pods-MDBMiniProject3-MDBMiniProject3UITests/Pods-MDBMiniProject3-MDBMiniProject3UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MDBMiniProject3_MDBMiniProject3UITests : NSObject
+@end
+@implementation PodsDummy_Pods_MDBMiniProject3_MDBMiniProject3UITests
+@end
